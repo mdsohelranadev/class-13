1fc656427f3d880b348e2f54aa0da89f9e42bef6
Sohel Rana <mdsohelrana191807@gmail.com>
2026-08-07 19:57:35 +0600

    oksx

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $('.bannersliderPart, .slides').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     prevArrow: ' <i class="fa-solid fa-circle-arrow-left prev"></i>',
     nextArrow: ' <i class="fa-solid fa-circle-arrow-right next"></i>'
 
